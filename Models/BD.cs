32cae4d74c2a350fe2006257dc582a0e2a0034cc
MICAELA YAEL CEMBAL <47297659@ort>
2022-07-06 12:11:32 -0300

    archivos

---
+++ Models/BD.cs
@@ -0,0 +1,7 @@
+/*Clase estática BD  con los siguientes métodos públicos (Crear todos los métodos privados que se requieran)
+AgregarJugador(Jugador Jug) Agrega el jugador a la base de datos.
+EliminarJugador(int IdJugador) Elimina el jugador de la base de datos.
+VerInfoEquipo(int IdEquipo): devuelve un objeto Equipo
+VerInfoJugador(int IdJugador): Devuelve un objeto Jugador
+ListarEquipos(): Devuelve un List de Equipos
+ListarJugadores(int IdEquipo): Devuelve un List de Jugadores*/
