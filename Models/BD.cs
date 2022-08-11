8a147f1f91e9538abbb0f37dc4aa37229bcd87cb
unknown <47297659@est.ort.edu.ar>
2022-08-11 15:31:01 -0300

    cambios

--- Models/BD.cs
+++ Models/BD.cs
@@ -14,7 +14,7 @@ namespace TP06.Models;
     public class BD {
 
  private static string _connectionString = 
-        @"Server=NOTEHP\SQLEXPRESS01;
+        @"Server=MICAELA-CEMBAL\SQLEXPRESS;
         DataBase=Qatar2022;Trusted_Connection=True;";
 
     /*AgregarJugador(Jugador Jug) Agrega el jugador a la base de datos.*/
